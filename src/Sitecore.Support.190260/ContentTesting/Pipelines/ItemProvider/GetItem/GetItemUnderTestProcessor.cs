2094bcfcaffc5352b1464c89ac96aa77e3811c52
SC-AFI <SC-AFI@users.noreply.github.com>
2017-10-24 15:01:19 +0300

    Fix

---
+++ src/Sitecore.Support.190260/ContentTesting/Pipelines/ItemProvider/GetItem/GetItemUnderTestProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Sitecore.Support.ContentTesting.Pipelines.ItemProvider.GetItem
+{
+  using Sitecore.ContentTesting;
+  public class GetItemUnderTestProcessor : Sitecore.ContentTesting.Pipelines.ItemProvider.GetItem.GetItemUnderTestProcessor
+  {
+    // allowCache = false;
+    public GetItemUnderTestProcessor() : this(false, null)
+    {
+    }
+
+    public GetItemUnderTestProcessor(bool allowCache, IContentTestingFactory factory = null): base(allowCache, factory)
+    {
+    }
+  }
+}
---
+++ src/Sitecore.Support.190260/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.190260")]
+[assembly: AssemblyProduct("Sitecore.Support.190260")]
+[assembly: ComVisible(false)]
